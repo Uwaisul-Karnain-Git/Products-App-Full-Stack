31380d7d97b09586014efebcd0b3f28df1dadde5
Uwaisul Karnain <78413138+Uwaisul-Karnain-Git@users.noreply.github.com>
2021-03-14 09:47:29 +0530

    Implement Bulk Delete

--- client/src/api/product.js
+++ client/src/api/product.js
@@ -4,8 +4,7 @@ const url='http://localhost:5000/product';
 
 export const fetchProducts = () => axios.get(url);
 export const fetchFilteredProducts = (name, price) => axios.get(`${url}/${name}/${price}`);
-//export const createProduct = (product) => axios.post(url, product);
-export const updatePostToDeleteState = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+//export const updateProductToDeleteState = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
 
 export const createProduct = (product) => axios.post(url, product, {
     headers: {
@@ -15,6 +14,15 @@ export const createProduct = (product) => axios.post(url, product, {
       console.log('Axios Response: ' + response);
 }).catch(error => {
       console.log(error.response);
-})
+});
 
+export const updateMultipleProductsToDeleteState = (ids) => axios.patch(url, ids, {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}).then(response => {
+      console.log('Axios Response: ' + response);
+}).catch(error => {
+      console.log(error.response);
+});
 
